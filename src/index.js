0bf9aa8bdca06a66a887e41a969ab505cf567875
mbibko <mbibko@gmail.com>
2022-11-02 14:36:12 +0200

    added: animate circle on each repaint, added: wrapper for circles

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
-const wrapper = document.body;
+const wrapper = document.querySelector(".wrapper");
 
-const MAX_STEP = 400;
+const MAX_STEP = 200;
+const SPEED = 5;
 
 let num = 0;
+const els = [];
 
-const tick = (el, step) => {
+const animate = (el, step) => {
     el.style.width = step * 1 + "px";
     el.style.height = step * 1 + "px";
     el.style.opacity = 1 - step / MAX_STEP;
 };
 
 const draw = (x, y, num) => {
-    let step = 0;
     const el = document.createElement("div");
     el.classList.add("el");
     el.textContent = num;
     el.style.left = x + "px";
     el.style.top = y + "px";
 
-    tick(el, step);
-
-    const interval = setInterval(() => {
-        if (step > MAX_STEP) {
-            clearInterval(interval);
-            el.remove();
-        }
-        tick(el, step);
-        step++;
-    }, 10);
+    els.push([el, performance.now()]);
+    console.log(els[0]);
     wrapper.appendChild(el);
 };
 
 wrapper.addEventListener("click", (e) => {
-    const [x, y] = [e.pageX, e.pageY];
+    const [x, y] = [e.offsetX, e.offsetY];
     draw(x, y, num++);
 });
+
+const tick = (timestamp) => {
+    els.map((item, index) => {
+        const [el, createdTime] = item;
+        const elapsed = timestamp - createdTime;
+        const count = Math.min(0.1 * elapsed * SPEED, MAX_STEP);
+        if (count === MAX_STEP) {
+            el.remove();
+            els.splice(index, 1);
+        }
+        animate(el, count);
+    });
+};
+
+const loop = () => {
+    window.requestAnimationFrame((timestamp) => {
+        tick(timestamp);
+        loop();
+    });
+};
+loop();
